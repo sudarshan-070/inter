c118ee35d5f59b3651225cd734db41da82abd84c
sudarshan070 <shindesudarshan070@gmail.com>
2023-03-23 22:08:10 +0530

    added js file and some images

---
+++ index.js
@@ -0,0 +1,74 @@
+console.log("i'm external file");
+// alert('Hello world!')
+console.log("Hello I'm in console")
+console.log(`hello Rishbh`);
+
+// single thered 
+
+// variable 
+
+// var, let const -
+
+// let 
+
+let name;  // let --> keyword, name --> variable name 
+
+let userName // userName --. variable name  // declearation
+
+name = 'Rishbh' // assign 
+
+name = "Pranav" // re-assign
+
+name = 'John'
+
+name = 'Dhiraj'
+
+name = 'Ram'
+
+// let name --  we can't re-declare variable name  with let key word
+
+console.log(name); //  Ram  
+
+let user = 'G'
+ 
+let message = 'Hello Im G'
+
+ console.log(message);
+
+//  const - constant 
+
+const pi = 3.14 
+const g = 9.8
+const BD = '11/08/2002'
+
+// BD = '24/01/2002' // can't reassign the constant 
+
+console.log(BD);
+
+
+// var 
+
+var myName = 'SRK'
+var myName = 'Suyan'  // variable name re-declare 
+console.log(myName); // 
+myName = 'Ritesh'
+myName = 'Omkar'
+console.log(myName); // 
+
+
+//var 1lll // first character not be a digit  
+// var _name , $name  for variable declaration we can use only '$' and '_'
+
+var name$username;
+
+// var user-username // getting error 
+
+
+// let imSitCollegeStudentPranavKale -- camelCase 
+
+let jsMessage = 'This is the js'
+let JSMessage = 'This is the js'  // js is case matter 
+
+// let, const, class, return, function, var, this   -- Reserved names 
+ 
+ 
